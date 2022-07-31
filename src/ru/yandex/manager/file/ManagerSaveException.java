8fd75834a614fbeb38ceec5595f12f7bbbb950ee
Ruslan8999 <rponomarenko89@yandex.ru>
2022-07-31 15:50:08 +0300

    Спринт 6, добавил ManagerSaveException

---
+++ src/ru/yandex/manager/file/ManagerSaveException.java
@@ -0,0 +1,8 @@
+package ru.yandex.manager.file;
+
+public class ManagerSaveException extends RuntimeException {
+
+    public ManagerSaveException(final String message) {
+        super(message);
+    }
+}
